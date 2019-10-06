c6469c50ae3a72c0feaf7481274c45c7e8c34706
Alexandre <alexandre.vielfaure@gmail.com>
2019-10-06 16:40:05 -0400

    TP2

---
+++ TP2/Lab_2.py
@@ -0,0 +1,72 @@
+"""
+Created on Mon Sep  9 15:12:19 2019
+
+@author: alexandre
+"""
+
+import numpy as np
+import math
+import copy
+from collections import deque
+
+class State:
+    
+    """
+    Contructeur d'un état initial
+    """
+    def __init__(self, pos):
+        """
+        pos donne la position de la voiture i dans sa ligne ou colonne (première case occupée par la voiture);
+        """
+        self.pos = np.array(pos)
+        
+        """
+        c,d et prev premettent de retracer l'état précédent et le dernier mouvement effectué
+        """
+        self.c = self.d = self.prev = None
+        
+        self.nb_moves = 0
+        self.score = 0
+        
+        # TODO
+        self.rock = None
+
+    """
+    Constructeur d'un état à partir du mouvement (c,d)
+    """
+    def move(self, c, d):
+        s = State(self.pos)
+        s.prev = self
+        s.pos[c] += d
+        s.c = c
+        s.d = d
+        s.nb_moves = self.nb_moves + 1
+        # TODO
+        return s
+
+    def put_rock(self, rock_pos):
+        # TODO
+        return None
+            
+    def score_state(self):
+        # TODO
+
+    def success(self):
+        return self.pos[0] == 4
+    
+    def __eq__(self, other):
+        if not isinstance(other, State):
+            return NotImplemented
+        if len(self.pos) != len(other.pos):
+            print("les états n'ont pas le même nombre de voitures")
+        
+        return np.array_equal(self.pos, other.pos)
+    
+    def __hash__(self):
+        h = 0
+        for i in range(len(self.pos)):
+            h = 37*h + self.pos[i]
+        return int(h)
+    
+    def __lt__(self, other):
+        return (self.score) < (other.score)
